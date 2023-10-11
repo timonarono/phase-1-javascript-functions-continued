522e2449bda86519b0af2e91787d14bbb0f95278
Timona Rono <timonarono@gmail.com>
2023-10-12 01:32:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return(`This Saturday, I want to ${activity}!`)
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+function mondayWork(activity='go to the office') {
+return(`This Monday, I will ${activity}.`)
+}
+mondayWork()
+
+function wrapAdjective( oparetor="*") {
+return function (inner="special"){
+    return(`You are ${oparetor}${inner}${oparetor}!`)
+}
+
+}
+wrapAdjective("||") 
